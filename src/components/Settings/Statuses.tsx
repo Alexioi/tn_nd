a8247ed91a6c269c768247703622e31a282d7c79
Alexey Ivanov <a@iva.nov.ru>
2026-04-21 22:53:45 +0300

    .

--- src/components/Settings/Statuses.tsx
+++ src/components/Settings/Statuses.tsx
@@ -1,30 +1,21 @@
-import { Button, Flex, Input } from "antd";
+import { Button, Flex, Input, Typography } from "antd";
+import { useState } from "react";
 
 import { useSettings } from "../../store";
 
 const Statuses = () => {
   const { statuses, setStatuses } = useSettings();
+  const [status, setStatus] = useState("");
 
   return (
     <Flex vertical gap={10} justify="center">
       {statuses.map((el, i) => {
         return (
-          <Flex gap={10}>
-            <Input
-              key={i}
-              onChange={(value) => {
-                const newStatuses = statuses.map((subEl, index) => {
-                  if (i === index) {
-                    return value.target.value;
-                  }
+          <Flex gap={10} align="center" key={el} justify="space-between">
+            <Typography.Text strong style={{ fontSize: "20px" }}>
+              {el}
+            </Typography.Text>
 
-                  return subEl;
-                });
-
-                setStatuses(newStatuses);
-              }}
-              value={el}
-            />
             <Button
               type="primary"
               danger
@@ -41,15 +32,22 @@ const Statuses = () => {
           </Flex>
         );
       })}
+      <Input
+        onChange={(value) => {
+          setStatus(value.target.value);
+        }}
+        value={status}
+      />
       <Button
         type="primary"
         onClick={() => {
-          setStatuses([...statuses, ""]);
+          setStatuses([...statuses, status]);
+          setStatus("");
         }}
         disabled={
           statuses.find((el) => {
-            return el === "";
-          }) !== undefined
+            return el === status;
+          }) !== undefined || status === ""
         }
       >
         добавить
